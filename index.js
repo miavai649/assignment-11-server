e27bb024ccc437a79577f2a4e7a17565e7868067
Mahamudul Haque Noor <mahamudulhaquenoor@gmail.com>
2022-11-10 19:51:26 +0600

    cors and express json connected

--- index.js
+++ index.js
@@ -14,7 +14,9 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@clu
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+async function run() {
 
+}
 
 app.get('/', (req, res) => {
     res.send('my server is running')
